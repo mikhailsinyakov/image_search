c363c62c3a6de2a81aae49e1f76eca79c747a290
Glitch (wide-drawer) <none>
2017-12-13 17:05:30 +0000

    📇🏜 Checkpoint server.js:586560

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ app.use(express.static('public'));
 
 const start = "https://www.googleapis.com/customsearch/v1?"
 const apikey = "key=AIzaSyAe8wZr-l9hc_lHlOUuzKeloLcEmPCsX2k";
-const searchEngine = "&cx=";
-const query = "&cat";
+const searchEngine = "&cx=015836358660699203191:ygc14bznb5q";
+const query = "&q=cat";
+const type = "&searchType=image";
 const numResults = "&num=10";
-const offset = "&start=0"
+const offset = "&start=1"
 const url = start + apikey + searchEngine + query + numResults + offset;
+request(url, (err, res, body) => {
+  console.log(body);
+});
 
 
 app.listen(port, function () {
