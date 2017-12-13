f53fb4febaa9cda95a2e2ef62fa8b0cf8bd99508
Glitch (wide-drawer) <none>
2017-12-13 14:23:29 +0000

    👘🎙 Checkpoint server.js:586560 package.json:586560

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-
+const apikey = "AIzaSyCL5Lj7n27PI6SsVAuybPf85wz5xeyBQLw";
 
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port);
