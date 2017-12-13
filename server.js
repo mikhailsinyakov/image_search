021763e770d936b72d49042bf467c27cc291fd00
Glitch (wide-drawer) <none>
2017-12-13 17:27:27 +0000

    ⛸🙎 Checkpoint server.js:586560

--- server.js
+++ server.js
@@ -5,22 +5,29 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-const start = "https://www.googleapis.com/customsearch/v1?"
+const requestBody = "https://www.googleapis.com/customsearch/v1?"
 const apikey = "key=AIzaSyAe8wZr-l9hc_lHlOUuzKeloLcEmPCsX2k";
 const searchEngine = "&cx=015836358660699203191:ygc14bznb5q";
 const query = "&q=cat";
 const type = "&searchType=image";
 const numResults = "&num=10";
-const offset = "&start=1"
+const offset = "&start=1";
 const url = start + apikey + searchEngine + query + type + numResults + offset;
-request(url, (err, res, body) => {
+/*request(url, (err, res, body) => {
   const obj = JSON.parse(body);
-  //const response = obj.items.map(val => {
-  //img_url: val.link,
-  //alt_text: val.snippet,
-  //page_url: val.image.contextLink
-//})
-  console.log(JSON.parse(body).items)
+  const response = obj.items.map(val => {
+    return {
+      img_url: val.link,
+      alt_text: val.snippet,
+      page_url: val.image.contextLink
+    }
+  });
+  console.log(response);
+});*/
+
+app.get("/api/imagesearch/:query", (req, res) => {
+  const query = req.params.query;
+  
 });
 
 
