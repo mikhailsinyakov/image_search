21ace413e37163ba0768455104a12f986f8a6f9d
Glitch (wide-drawer) <none>
2017-12-13 17:38:27 +0000

    👼🎬 Checkpoint server.js:586560

--- server.js
+++ server.js
@@ -5,14 +5,8 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-const requestBody = "https://www.googleapis.com/customsearch/v1?"
-const apikey = "key=AIzaSyAe8wZr-l9hc_lHlOUuzKeloLcEmPCsX2k";
-const searchEngine = "&cx=015836358660699203191:ygc14bznb5q";
-const query = "&q=cat";
-const type = "&searchType=image";
-const numResults = "&num=10";
-const offset = "&start=1";
-const url = start + apikey + searchEngine + query + type + numResults + offset;
+const requestBody = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAe8wZr-l9hc_lHlOUuzKeloLcEmPCsX2k&cx=015836358660699203191:ygc14bznb5q&searchType=image&num=10"
+
 /*request(url, (err, res, body) => {
   const obj = JSON.parse(body);
   const response = obj.items.map(val => {
@@ -20,14 +14,19 @@ const url = start + apikey + searchEngine + query + type + numResults + offset;
       img_url: val.link,
       alt_text: val.snippet,
       page_url: val.image.contextLink
-    }
+    };
   });
   console.log(response);
 });*/
 
 app.get("/api/imagesearch/:query", (req, res) => {
-  const query = req.params.query;
-  
+  console.log(req.params.query);
+  let query = req.params.query.match(//);
+  query = "&q=" + query;
+  const param = "";
+  const offset = "&start=1";
+  const url = requestBody + query + offset;
+  res.send("OK")
 });
 
 
